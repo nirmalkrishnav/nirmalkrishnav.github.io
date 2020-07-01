3c974074193437d359a608b72e7ed65a9858de36
meuequalsD <nirmalkrishnav@gmail.com>
2020-07-02 00:31:21 +0530

    App strucuture chagnes for using git hub pages

---
+++ build/precache-manifest.54674504dd262693de4998b02dee9e03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a7d07a51ed70abb38bf0e7beeaaa46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "728dc38a3bd366971037",
+    "url": "/static/css/main.159ce419.chunk.css"
+  },
+  {
+    "revision": "ccc90f0f9d8f06821b12",
+    "url": "/static/js/2.79cc8be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79cc8be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728dc38a3bd366971037",
+    "url": "/static/js/main.19fc4185.chunk.js"
+  },
+  {
+    "revision": "02c37c0bef9b805a76f9",
+    "url": "/static/js/runtime-main.7a3fef95.js"
+  },
+  {
+    "revision": "f53ebb7bc7eb4957d6aa13867cb66f42",
+    "url": "/static/media/GothicA1-Bold.f53ebb7b.ttf"
+  },
+  {
+    "revision": "e742f688ceac24b94fedc826b19a43df",
+    "url": "/static/media/GothicA1-Regular.e742f688.ttf"
+  }
+]);
