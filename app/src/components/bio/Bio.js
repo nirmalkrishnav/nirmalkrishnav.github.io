349000a712b20379766f68dbfe1ff832a8c63f3e
meuequalsD <nirmalkrishnav@gmail.com>
2020-07-02 00:49:18 +0530

    bio changes

--- app/src/components/bio/Bio.js
+++ app/src/components/bio/Bio.js
@@ -3,24 +3,23 @@ import React from 'react';
 function Bio() {
     return (
         <div>
-
-        <p>
-            I’m a Software Engineer currently working in EY. I mostly do front-end development with Angular and React.
-            I love javascript, but Typescript is my Fwb.
-           
-        </p>
-        <ul className="nav social-links">
-            <li className="nav-item">
-                <a className="nav-link" href="https://github.com/nirmalkrishnav">GitHub</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="https://twitter.com/meuequalsD">Twitter</a>
-            </li>
-            <li className="nav-item">
-                <a className="nav-link" href="https://www.linkedin.com/in/nirmal-krishna/">LinkedIn</a>
-            </li>
-        </ul>
-        </div>  
+            <p>
+                I’m a Software Engineer currently working with EY.<br />
+                I love javascript. But, Typescript is my Fwb.<br/>
+                🐍 for backend, coffee for <code>!</code>tiredness.
+            </p>
+            <ul className="nav social-links">
+                <li className="nav-item">
+                    <a className="nav-link" href="https://github.com/nirmalkrishnav">GitHub</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="https://twitter.com/meuequalsD">Twitter</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="https://www.linkedin.com/in/nirmal-krishna/">LinkedIn</a>
+                </li>
+            </ul>
+        </div>
     )
 }
 
